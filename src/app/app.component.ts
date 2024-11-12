a862ff02b28ce868ca4b04fb5dfa96e6eb9b7f11
RostislavLyskun <99643719+RostislavLyskun@users.noreply.github.com>
2024-11-12 13:05:17 +0200

    v6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
