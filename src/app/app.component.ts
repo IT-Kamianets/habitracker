096cd0b82ed2adb957931e023aad56839790b2a2
RostislavLyskun <99643719+RostislavLyskun@users.noreply.github.com>
2024-11-12 12:49:09 +0200

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,25 +11,3 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'habittracker';
 }
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'; // імпортуємо FormsModule
-
-import { AppComponent } from './app.component';
-import { HabitTrackerComponent } from './habit-tracker/habit-tracker.component';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    HabitTrackerComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule // додаємо FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
-
-{}
